1d79b402e67a00956a4300b02138b4ccb00b55fb
michelcias <michelcias@gmail.com>
2025-04-18 01:28:15 -0300

    Added the function generate_precision_data, to draw from the full conditional distribution of data precision.

--- src/conditional_precision.c
+++ src/conditional_precision.c
@@ -3,6 +3,60 @@
 #include "conditional_precision.h"
 
 /**
+ * Generates a sample from the full conditional posterior distribution of the data precision 1/V
+ * within a Gibbs sampler iteration.
+ *
+ * This function updates the precision associated with the observation noise (data precision)
+ * for the current MCMC iteration (`iter`). It assumes a Gamma posterior distribution,
+ * derived from a Gamma prior and a Normal likelihood for the data.
+ *
+ * Calculation steps:
+ * 1. Computes the sum of squared residuals (`ss_y`): \sum_{i=1}^n (y_i - theta_1_post[i])^2
+ * 2. Updates the shape parameter (`nu_y_post`) of the Gamma posterior: prior shape + n/2.
+ * 3. Updates the rate parameter (`eta_y_post`) of the Gamma posterior: prior rate + ss_y/2.
+ * 4. Samples a new value for the data precision `prec_y_post[iter]` from the resulting
+ *    Gamma(shape, scale=1/rate) distribution using rgamma().
+ *
+ * @param y              Array of observed data (length n).
+ * @param theta_1_post   Array storing posterior samples of theta_1 (vectorized B x n matrix).
+ *                       Uses the vector corresponding to `iter`. Elements indexed 0 to n-1.
+ * @param prec_y_post    Output array storing posterior samples of the data precision 1/V.
+ *                       The value at index `iter` will be updated.
+ * @param nu_y           Prior shape parameter for the Gamma distribution of 1/V.
+ * @param eta_y          Prior rate parameter for the Gamma distribution of 1/V.
+ * @param n              Sample size, representing the number of elements (0 to n-1).
+ * @param iter           Current MCMC iteration index (0-based).
+ */
+void generate_precision_data(double *y,
+                             double *theta_1_post,
+                             double *prec_y_post,
+                             double nu_y,
+                             double eta_y,
+                             int n,
+                             int iter) {
+
+  int i;
+  int iter_n = iter * n; // Offset for current iteration's data
+
+  // Compute sum of squared residuals
+  double ss_y = 0.0;
+  for (i = 0; i < n; i++) {
+    double resid = y[i] - theta_1_post[iter_n + i];
+    ss_y += resid * resid;
+  }
+
+  // Update Gamma posterior parameters
+  double nu_y_post = nu_y + n / 2.0;
+  double eta_y_post = eta_y + ss_y / 2.0;
+
+  // Sample data precision 1/V from Gamma(shape, scale) distribution
+  // R's rgamma uses scale = 1 / rate
+  prec_y_post[iter] = rgamma(nu_y_post, 1.0 / eta_y_post);
+}
+
+//----------------------------------------------------------------------
+
+/**
  * Generates a sample from the full conditional posterior distribution of the precision 1/W_k
  * (for indices k from 1 to p-1) within a Gibbs sampler iteration.
  *

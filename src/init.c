3dc0fd9177c92218b711b208d271f546db4f2076
michelcias <michelcias@gmail.com>
2025-04-19 03:17:21 -0300

    Registered native routine for the new mcmc_localtrend function.

--- src/init.c
+++ src/init.c
@@ -8,10 +8,29 @@
  * The CallEntries array defines the mapping between R-callable routine names and
  * their corresponding C functions and argument counts.
  */
-SEXP C_ILogit(SEXP x); // Declaration of the wrapper implemented in utils.c
+
+/* Declaration of the existing wrapper in utils.c */
+SEXP C_ILogit(SEXP x);
+
+/* Declaration of the new MCMC local-trend sampler */
+SEXP C_MCMC_localtrend(
+    SEXP y_,
+    SEXP burnin_,
+    SEXP thinning_,
+    SEXP n_chain_,
+    SEXP prior_theta01_mean_,
+    SEXP prior_theta01_prec_,
+    SEXP prior_prec1_shape_,
+    SEXP prior_prec1_rate_,
+    SEXP prior_prec_y_shape_,
+    SEXP prior_prec_y_rate_
+);
 
 static const R_CallMethodDef CallEntries[] = {
-  {"_pdm_ilogit", (DL_FUNC) &C_ILogit, 1},
+  /* existing ilogit wrapper */
+  {"_pdm_ilogit",           (DL_FUNC) &C_ILogit,               1},
+  /* new Gibbs sampler for local-trend */
+  {"_pdm_mcmc_localtrend",  (DL_FUNC) &C_MCMC_localtrend,      10},
   {NULL, NULL, 0}
 };
 

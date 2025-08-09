921cf2968a3710b4d0bc7fb4850898297773467c
michelcias <michelcias@gmail.com>
2025-08-09 17:59:56 -0300

    Add debug validation to generate_normal_vector function in utils.c

--- src/utils.c
+++ src/utils.c
@@ -1,6 +1,7 @@
-// This block validates the input for the DEBUG mode, ensuring that the generated normal vector meets the required criteria.
+#include <R.h>
+#include <Rmath.h>
+#include <Rinternals.h>
 
-<<<<<<< HEAD
 /**
  * Computes the inverse logit (logistic) function of the input value.
  *
@@ -129,7 +130,3 @@ SEXP C_ILogit(SEXP x) {
   double val = ilogit(REAL(x)[0]);
   return ScalarReal(val);
 }
-=======
-    // DEBUG validation block
-    if (debug) { /* existing code */ }
->>>>>>> 4285767d7607291e8a4060c4682a2f809c830cf0

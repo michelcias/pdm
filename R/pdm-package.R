b40b1db3739e991129d62382cfb14914e266fd39
michelcias <michelcias@gmail.com>
2026-07-05 02:29:22 -0300

    ```
    chore: format R package imports and ensure EOL newline
    
    - Consolidate `graphics` package imports into a single line.
    - Ensure a newline character at the end of the file.
    ```

--- R/pdm-package.R
+++ R/pdm-package.R
@@ -4,8 +4,7 @@
 ## usethis namespace: start
 #' @useDynLib pdm, .registration = TRUE
 #' @importFrom stats acf density median qnorm quantile sd
-#' @importFrom graphics abline axTicks axis grid legend lines mtext par
-#'   points polygon segments title
+#' @importFrom graphics abline axTicks axis grid legend lines mtext par points polygon segments title
 #' @importFrom utils tail
 ## usethis namespace: end
 NULL
